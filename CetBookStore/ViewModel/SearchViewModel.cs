410d813b0c3d82148e7efc5186d5d2c68d530c7e
TARO TERZİYAN <tarocimbom@hotmail.com>
2020-04-30 02:25:34 +0300

    Add project files.

---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+        [Display(Name = "Aramak için buraya yazın ")]
+        public string SearchText { get; set; }
+        [Display(Name = "Açıklamalarda aramak için buraya tıklayın")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name = "Kategoriler")]
+        public int? CategoryId { get; set; }
+        [Display(Name = "En düşük fiyatı girin ")]
+        public decimal? MinPrice { get; set; }
+        [Display(Name = "En yüksek fiyatı girin")]
+        public decimal? MaxPrice { get; set; }
+
+        public List<Models.Book> Results { get; set; }
+    }
+}
